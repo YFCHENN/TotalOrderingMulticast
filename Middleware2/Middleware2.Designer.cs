988d2a571db593f78ab58efa75484d35a46c11f4
YFCHENN <chenyeefei90@gmail.com>
2019-11-20 14:46:01 +1300

    v.1.0.0 with no bug

---
+++ Middleware2/Middleware2.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Middleware2
+{
+    partial class Middleware2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendButton = new System.Windows.Forms.Button();
+            this.labelSent = new System.Windows.Forms.Label();
+            this.labelReady = new System.Windows.Forms.Label();
+            this.labelReceived = new System.Windows.Forms.Label();
+            this.listBoxSent = new System.Windows.Forms.ListBox();
+            this.listBoxReceived = new System.Windows.Forms.ListBox();
+            this.listBoxReady = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendButton.Location = new System.Drawing.Point(57, 76);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(141, 36);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // labelSent
+            // 
+            this.labelSent.AutoSize = true;
+            this.labelSent.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSent.Location = new System.Drawing.Point(53, 170);
+            this.labelSent.Name = "labelSent";
+            this.labelSent.Size = new System.Drawing.Size(47, 22);
+            this.labelSent.TabIndex = 1;
+            this.labelSent.Text = "Sent";
+            // 
+            // labelReady
+            // 
+            this.labelReady.AutoSize = true;
+            this.labelReady.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReady.Location = new System.Drawing.Point(820, 170);
+            this.labelReady.Name = "labelReady";
+            this.labelReady.Size = new System.Drawing.Size(62, 22);
+            this.labelReady.TabIndex = 2;
+            this.labelReady.Text = "Ready";
+            // 
+            // labelReceived
+            // 
+            this.labelReceived.AutoSize = true;
+            this.labelReceived.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReceived.Location = new System.Drawing.Point(435, 170);
+            this.labelReceived.Name = "labelReceived";
+            this.labelReceived.Size = new System.Drawing.Size(85, 22);
+            this.labelReceived.TabIndex = 3;
+            this.labelReceived.Text = "Received";
+            // 
+            // listBoxSent
+            // 
+            this.listBoxSent.FormattingEnabled = true;
+            this.listBoxSent.ItemHeight = 20;
+            this.listBoxSent.Location = new System.Drawing.Point(57, 232);
+            this.listBoxSent.Name = "listBoxSent";
+            this.listBoxSent.Size = new System.Drawing.Size(351, 324);
+            this.listBoxSent.TabIndex = 4;
+            // 
+            // listBoxReceived
+            // 
+            this.listBoxReceived.FormattingEnabled = true;
+            this.listBoxReceived.ItemHeight = 20;
+            this.listBoxReceived.Location = new System.Drawing.Point(439, 232);
+            this.listBoxReceived.Name = "listBoxReceived";
+            this.listBoxReceived.Size = new System.Drawing.Size(351, 324);
+            this.listBoxReceived.TabIndex = 5;
+            // 
+            // listBoxReady
+            // 
+            this.listBoxReady.FormattingEnabled = true;
+            this.listBoxReady.ItemHeight = 20;
+            this.listBoxReady.Location = new System.Drawing.Point(824, 232);
+            this.listBoxReady.Name = "listBoxReady";
+            this.listBoxReady.Size = new System.Drawing.Size(351, 324);
+            this.listBoxReady.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1230, 671);
+            this.Controls.Add(this.listBoxReady);
+            this.Controls.Add(this.listBoxReceived);
+            this.Controls.Add(this.listBoxSent);
+            this.Controls.Add(this.labelReceived);
+            this.Controls.Add(this.labelReady);
+            this.Controls.Add(this.labelSent);
+            this.Controls.Add(this.sendButton);
+            this.Name = "Form1";
+            this.Text = "Middleware2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Label labelSent;
+        private System.Windows.Forms.Label labelReady;
+        private System.Windows.Forms.Label labelReceived;
+        private System.Windows.Forms.ListBox listBoxSent;
+        private System.Windows.Forms.ListBox listBoxReceived;
+        private System.Windows.Forms.ListBox listBoxReady;
+    }
+}
+
---
+++ Middleware2/Middleware2.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Middleware2
+{
+
+
+    public partial class Middleware2 : Form
+    {
+        private int count = 1;
+        private int clock = 0;
+        private string id = "Middleware 2";
+        private string idAbre = "M2";
+        private List<int>[] ackSeq = new List<int>[100];
+
+        private class MSG
+        {
+            public string Msg { get; set; }
+            public string Msgid { get; set; }
+            public string Middlewareid { get; set; }
+            public int ProposedTimeStamp { get; set; }
+            public Boolean Deliverable { get; set; }
+        }
+        private List<MSG> msgSeq = new List<MSG>();
+
+        // private string finalAck; 
+        public Middleware2()
+        {
+            InitializeComponent();
+            ReceiveMulticast();
+
+        }
+        int myPort = 8083;
+
+        private async void ReceiveMulticast()
+        {
+
+            /*  ackSeq[0] = new List<int>();
+              ackSeq[1] = new List<int>();
+              ackSeq[2] = new List<int>();
+              ackSeq[3] = new List<int>();
+              ackSeq[4] = new List<int>(); */
+            byte[] bytes = new Byte[1024];
+
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = null;
+            foreach (IPAddress ip in ipHostInfo.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip;
+                    break;
+                }
+            }
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, myPort);
+
+            TcpListener listener = new TcpListener(localEndPoint);
+            listener.Start(10);
+            try
+            {
+                string data = null;
+                //Waiting for incoming connection forever
+                while (true)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    data = null;
+
+                    //wait for incomming msg
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        NetworkStream stream = client.GetStream();
+                        int bytesRead = await stream.ReadAsync(bytes, 0, 1024);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRead);
+
+                        //to depend the reaction on what the received msg is 
+                        if (data.IndexOf("EOM") > -1)
+                        {
+                            //if the message is an ACK message
+                            if (data.IndexOf("ACK") > -1)
+                            {
+                                //if the sender receive the ACK,  send a FinACK msg if #seq >=5, else append it to the ackSeq list
+                                if (data.IndexOf(idAbre) > -1)
+                                {
+                                    string timeStamp = GetTimeStamp(data);
+                                    string[] time = timeStamp.Split(',');
+                                    string msgId = GetMsgId(data, 2, 3);
+                                    int ackIndex = Int32.Parse(msgId.Substring(msgId.IndexOf("#") + 1));
+                                    if (ackSeq[ackIndex] == null)
+                                    {
+                                        ackSeq[ackIndex] = new List<int>();
+                                    }
+                                    ackSeq[ackIndex].Add(Int32.Parse(time[0]));
+                                    if (ackSeq[ackIndex].Count >= 5)
+                                    {
+                                        int agreedTime = ackSeq[ackIndex].Max();
+                                       //string msgId = GetMsgId(data, 2, 3);
+                                        string finalAck = "FinAck for " + msgId + " from " + id + ": (" + agreedTime + "," + idAbre + ") <EOM>\n";
+                                        bytes = Encoding.ASCII.GetBytes(finalAck);
+                                        SendMsg(bytes);
+                                        ackSeq[ackIndex].Clear();
+                                    }
+
+                                    //parse the                                                                               
+
+                                    stream.Close();
+                                    break;
+                                }
+                                //if the receiver receive ACK msg, drop it and continue listening for income msg
+                                else
+                                {
+                                    break;
+                                }
+                            }
+
+                            //if the message is an FinACK msg
+                            else if (data.IndexOf("FinAck") > -1)
+                            {
+                                //need to update this line
+                                string timeStamp = GetTimeStamp(data);
+                                string[] time = timeStamp.Split(',');
+                                string msgId = GetMsgId(data, 2, 3);
+                                int index = msgSeq.FindIndex(item => item.Msg.Contains(time[1]) && item.Msg.Contains(msgId));
+                                // MessageBox.Show(id + "count: " + msgSeq.Count.ToString() + "\n" + "index: " + index);
+                                msgSeq[index].ProposedTimeStamp = Int32.Parse(time[0]);
+                                msgSeq[index].Deliverable = true;
+                                msgSeq.Sort(delegate (MSG x, MSG y)
+                                {
+                                    if (x.ProposedTimeStamp == y.ProposedTimeStamp)
+                                    {
+                                        if (Int32.Parse(x.Middlewareid.Substring(1)) < Int32.Parse(y.Middlewareid.Substring(1)))
+                                        { return -1; }
+                                        else if (Int32.Parse(x.Middlewareid.Substring(1)) == Int32.Parse(y.Middlewareid.Substring(1)))
+                                        {
+                                            if (Int32.Parse(x.Msgid.Substring(5)) < Int32.Parse(y.Msgid.Substring(5)))
+                                            {
+                                                return -1;
+                                            }
+                                            else return 1;
+                                        }
+                                        else return 1;
+                                    }
+                                    else
+                                    {
+                                        return x.ProposedTimeStamp.CompareTo(y.ProposedTimeStamp);
+                                    }
+
+                                });
+                                while (true)
+                                {
+                                    if (msgSeq.Count != 0 && msgSeq[0].Deliverable == true)
+                                    {
+                                        listBoxReady.Items.Add(msgSeq[0].Msg);
+                                        msgSeq.RemoveAt(0);
+
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+
+                            }
+                            else
+                            {
+                                //in case the data redandunt 
+                                if (listBoxReceived.Items.IndexOf(data) > -1) { break; }
+                                listBoxReceived.Items.Add(data);
+                                string timeStamp = GetTimeStamp(data);
+                                //update local logical clock when receiving a message
+                                string[] time = timeStamp.Split(',');
+                                clock = Math.Max(Int32.Parse(time[0]), clock) + 1;
+                                string msgId = GetMsgId(data, 0, 1);
+                                // msgSeq.Add(data + "[undeliverable]");
+                                string ack = "ACK for " + msgId + " from " + id + ": (" + clock + "," + time[1] + ") <EOM>\n";
+                                msgSeq.Add(new MSG() { Msg = data, Msgid = msgId, Middlewareid = time[1], ProposedTimeStamp = clock, Deliverable = false });
+                                bytes = Encoding.ASCII.GetBytes(ack);
+                                SendMsg(bytes);
+                                break;
+
+                            }
+                            stream.Close();
+                            break;
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception e)
+            { MessageBox.Show(e.ToString()); }
+        }
+
+        public string GetTimeStamp(string data)
+        {
+            int timeStampStart = data.IndexOf("(");
+            int timeStampEnd = data.IndexOf(")");
+            string timeStamp = data.Substring(timeStampStart + 1, timeStampEnd - timeStampStart - 1);
+            return timeStamp;
+        }
+
+        public string GetMsgId(string data, int m, int n)
+        {
+            string[] finAck = data.Split(' ');
+            string msgId = finAck[m] + " " + finAck[n];
+            return msgId;
+        }
+
+        public void SendMsg(byte[] bytes)
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = null;
+                foreach (IPAddress ip in ipHostInfo.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddress = ip;
+                        break;
+                    }
+                }
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8081);
+                Socket sendSocket;
+                try
+                {
+                    // Create a TCP/IP  socket.
+                    sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    // Connect to the Network 
+                    sendSocket.Connect(remoteEP);
+
+                    // Send the data to the network.
+                    int bytesSent = sendSocket.Send(bytes);
+
+
+
+                    sendSocket.Shutdown(SocketShutdown.Both);
+                    sendSocket.Close();
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+
+        public void DoWork()
+        {
+            //send ACK
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = null;
+                foreach (IPAddress ip in ipHostInfo.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddress = ip;
+                        break;
+                    }
+                }
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8081);
+                Socket sendSocket;
+                try
+                {
+                    // Create a TCP/IP  socket.
+                    sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    // Connect to the Network 
+                    sendSocket.Connect(remoteEP);
+
+
+                    string msgStr = "Msg #" + count + " from " + id + ": (" + clock + "," + idAbre + ") <EOM>\n";
+                    count++;
+                    listBoxSent.Items.Add(msgStr);
+                    byte[] msg = Encoding.ASCII.GetBytes(msgStr);
+
+                    // Send the data to the network.
+                    byte[] bytes = new byte[256];
+                    int bytesSent = sendSocket.Send(msg);
+                    clock++;
+
+                    sendSocket.Shutdown(SocketShutdown.Both);
+                    sendSocket.Close();
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+
+        }
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            DoWork();
+
+        }
+
+    }
+
+}
+
